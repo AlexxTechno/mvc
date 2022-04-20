16029106947e999865c8cd5368bcc3b2e22a256a
AlexxTechno <aalexx.007@mail.ru>
2022-04-20 15:04:06 +0300

    Удалил лишнее: Интерфейсы IAllCatalog.cs IAllCatalogItems.cs Репозитории CatalogItemRepository.cs CatalogRepository.cs Модели Catalog CatalogItem

--- mvc/Models/ProductCategory.cs
+++ mvc/Models/ProductCategory.cs
@@ -14,42 +14,5 @@ namespace mvc.Models
     {
         public Product Prod { get; set; }
         public IEnumerable<Category> Cat { get; set; }
-    }
-
-    public class Catalog
-    {
-        public Category Cat { get; set; }
-        public IEnumerable<Product> Prod { get; set; }
-    }
-    
-    public class CatalogItem
-    {
-        /*
-        public Category Cat { get; set; }
-        public IEnumerable<Product> Prod { get; set; }
-        public IEnumerable<Image> Img { get; set; }
-        public IEnumerable<Video> Video { get; set; }      
-        */       
-
-        public int CategoryId;
-        public int CategoryNumber;
-        public string CategoryTitle;
-        public string CategoryDesc;
-
-        public int ProductId;
-        public string ProductSku;
-        public string ProductTitle;
-        public bool ProductIsNew;
-        public string ProductDimension;
-        public int ProductPrice;
-        public int ProductPriceBig;
-        public int PrId;
-
-        public int ImageId;
-        public string ImageGuide;
-
-        public string VideoGuide;
-
-        internal object AllCatalogItems;
-    }
+    }    
 }
